68256987bdf3c8bb7c63d6070bbbfc7ab7321251
ajain <ajain@palo-it.com>
2024-09-24 15:26:27 +0800

    clip path corrected

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.fallback = {
+            ...config.resolve.fallback, // preserve existing fallbacks
+            fs: false, // Disable 'fs' for client-side
+        };
+
+        return config;
+    },
+};
 
 export default nextConfig;
+
