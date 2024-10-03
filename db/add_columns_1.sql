37b3e352e18aabfc438a96319aba5e7a1d30cf7b
ajain <ajain@palo-it.com>
2024-10-03 11:06:57 +0800

    new db changes

---
+++ db/add_columns_1.sql
@@ -0,0 +1,14 @@
+ALTER TABLE security_employees.booking_schedules 
+    ADD COLUMN stripe_session_id VARCHAR(255) NULL,
+    ADD COLUMN stripe_payment_id VARCHAR(255) NULL;
+
+ALTER TABLE security_employees.booking_schedules 
+    ADD COLUMN TR_NOTA VARCHAR(3) NULL,
+    ADD COLUMN TR_OBSE VARCHAR(3) NULL,
+    ADD COLUMN TR_SSM VARCHAR(3) NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX stripeid_UNIQUE ON security_employees.booking_schedules(stripe_session_id);
+
+-- CreateIndex
+CREATE UNIQUE INDEX stripepaymentid_UNIQUE ON security_employees.booking_schedules(stripe_payment_id);
