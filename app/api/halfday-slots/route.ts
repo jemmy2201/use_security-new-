117fe4f1e053c5dcf42107fe47195e023fa9b2af
ajain <ajain@palo-it.com>
2024-10-21 12:56:57 +0800

    halfday slots API

---
+++ app/api/halfday-slots/route.ts
@@ -0,0 +1,38 @@
+
+import { booking_schedules, PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+import { getEncryptedNricFromSession } from '../../../lib/session';
+
+const prisma = new PrismaClient();
+
+export async function GET(request: NextRequest) {
+
+
+  try {
+
+    const encryptedNric = await getEncryptedNricFromSession(request);
+    if (encryptedNric instanceof NextResponse) {
+      return encryptedNric;
+    }
+
+
+    const holidays = await prisma.dateholidays.findMany({
+      where: {
+        time_work: '2',
+      },
+      select: {
+        date: true,
+      },
+    });
+    console.log('halfday slots: ', holidays.map(h => h.date));
+    return NextResponse.json(holidays.map(h => h.date));
+
+  } catch (error) {
+    console.error('Error fetching halfday dates:', error);
+    return NextResponse.json({ error: 'Failed to fetch halfday dates' }, { status: 500 });
+  }
+
+}
+
+
+
