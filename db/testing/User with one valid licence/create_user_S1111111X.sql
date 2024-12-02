69ffd2faa56ded02312b554e54f53181457694c3
Jeremy Chu <jchu@palo-it.com>
2024-12-02 23:36:33 +0800

    Added SQL testing scripts

---
+++ db/testing/User with one valid licence/create_user_S1111111X.sql
@@ -0,0 +1,9 @@
+-- User (NRIC) = S1111111X, password: 123123
+INSERT INTO security_employees.users
+      (id, name, email, password, nric, mobileno, created_at, updated_at)
+   values 
+     ((SELECT max(uu.id) + 1 FROM security_employees.users uu),
+     'Tester Two','S1111111X@admin.com',
+     '$2y$10$B9b8MzrKuTHEejOkcE9azeJA5gVKJ9F6VCZEDEiqhvfbF55ap4Wk2',
+     'b2lrOGRaeHF2aEtHR2FHdTluSUZJdz09',
+     '6581392712','2024-07-31 06:26:05','2024-08-07 02:58:56');
---
+++ db/testing/User with two valid licence/create_1_record_for_valid_licence_for_SO_application.sql
@@ -0,0 +1,25 @@
+-- Creating 1 record for vald license for SO application
+
+INSERT INTO security_employees.booking_schedules
+   (id, app_type, card_id, grade_id, expired_date, nric, passid, created_at, licence_status, card_issue) 
+values 
+   ((SELECT max(uu.id) + 1 FROM security_employees.booking_schedules uu), 
+    '1', '1', '3', 
+    '22/12/2026', 
+    'eTUrbnB0cGQzV01RWTRFVHRWcEJ6UT09', 
+    '202411111BSO', 
+    '2024-07-31 14:32:22', 
+    'Y', 
+    '');
+
+INSERT INTO security_employees.booking_schedules
+   (id, app_type, card_id, grade_id, expired_date, nric, passid, created_at, licence_status, card_issue) 
+values 
+   ((SELECT max(uu.id) + 1 FROM security_employees.booking_schedules uu), 
+    '1', '3', '3', 
+    '22/12/2026', 
+    'eTUrbnB0cGQzV01RWTRFVHRWcEJ6UT09', 
+    '202411111BSO', 
+    '2024-07-31 14:32:22', 
+    'Y', 
+    '');    
