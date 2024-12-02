0b0426500442d3973684bee3d51027f88d27e729
Jeremy Chu <jchu@palo-it.com>
2024-12-03 00:00:13 +0800

    Formatting of message for expired or pending approval.  Also, card limitations message

--- app/components/model/Modal.tsx
+++ app/components/model/Modal.tsx
@@ -8,11 +8,13 @@ interface ModalProps {
   onClose: () => void;
 }
 
+// message=='2' means ID Card is not due for renewal 
+// message=='3' means Security licence may have expired or pending approval.
+
 const Modal: React.FC<ModalProps> = ({ message, onClose }) => {
   return (
     <div className={styleCss.modaloverlay}>
       <div className={styleCss.modelContainer}>
-
         {message && message == '2' ? (
           <>
             <div className={styleCss.modelBox}>
@@ -41,7 +43,8 @@ const Modal: React.FC<ModalProps> = ({ message, onClose }) => {
               Your security licence may have expired or pending approval.
               </div>
               <div className={globalStyleCss.regular}>
-              For successful application, it may take  48 -72 hours for your approved data from the Police Licensing and Regulatory Department (PLRD) to be updated on USE's ID card portal. Please try again later.
+              For successful application, it may take  48 -72 hours for your approved data from the Police Licensing and Regulatory Department (PLRD) to be updated on USE's ID card portal. <br/>
+              Please try again later.
               </div>
               <div className={styleCss.modelButton}>
                 <button className={globalStyleCss.buttonTextBlack} onClick={onClose}>
@@ -59,10 +62,10 @@ const Modal: React.FC<ModalProps> = ({ message, onClose }) => {
           <>
             <div className={styleCss.modelBox}>
               <div className={globalStyleCss.header2}>
-                ID card application limitations
+                ID Card Application Advisory
               </div>
               <div className={globalStyleCss.regular}>
-                Security Officers who already hold an Aviation Security Officer (AVSO) ID card and Private investigator (PI) ID card are not eligible to apply for additional ID Cards.
+              If you already have a Security Officer (SO) ID card and Private Investigator (PI) ID card, you are not eligible to apply for other ID cards.
               </div>
               <div className={styleCss.modelButton}>
                 <button className={globalStyleCss.buttonTextBlack} onClick={onClose}>
