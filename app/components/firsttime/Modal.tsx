27986845abd1f8ca9b81378a3c6aa93ba7317504
Jeremy Chu <jchu@palo-it.com>
2024-12-02 18:05:34 +0800

    Updated text for ID Card Application Limits modal

--- app/components/firsttime/Modal.tsx
+++ app/components/firsttime/Modal.tsx
@@ -14,10 +14,10 @@ const Modal: React.FC<ModalProps> = ({ message, onClose }) => {
       <div className={styleCss.modelContainer}>
         <div className={styleCss.modelBox}>
           <div className={globalStyleCss.header2}>
-          We noticed you do not have a valid card license or your licence may have expired.
+          Your security licence may have expired or pending approval.
           </div>
           <div className={globalStyleCss.regular}>
-          It may take 48-72 hours for your approved data from Police Licensing and Regulatory Department (PLRD) to be updated on USE&apos;s ID card portal. Please try again later.
+          For successful application, it may take  48 -72 hours for your approved data from the Police Licensing and Regulatory Department (PLRD) to be updated on USE's ID card portal. Please try again later.
           </div>
           <div className={styleCss.modelButton}>
             <button className={globalStyleCss.buttonTextBlack} onClick={onClose}>
