62af7d7baff66b177e0cdf52f07027b9820104cf
Balaji Srinivasan <bsrinivasan@palo-it.com>
2025-05-01 16:13:59 +0800

    feat(page):login page figma change

--- app/components/contactuslogin/page.tsx
+++ app/components/contactuslogin/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
@@ -8,92 +8,108 @@ import FooterPageLink from '../footer/FooterPage';
 import HeaderPageLink from '../header/HeaderPage';
 import { signIn } from 'next-auth/react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const ContactusPage: React.FC = () => {
+  const router = useRouter();
 
-    const router = useRouter();
+  const handleLoginClick = async () => {
+    signIn('singpass');
+  };
 
-    const handleLoginClick = async () => {
-        signIn('singpass');
-    };
+  const handleClick = () => {
+    router.push('/homepage');
+  };
 
-    const handleClick = () => {
-        router.push('/homepage');
-    };
-
-    return (
-
-        <form>
-
-            <div >
-                <HeaderPageLink />
+  return (
+    <form>
+      <div>
+        <HeaderPageLink />
+      </div>
+      <div className={contactusContentstyles.mainContainer}>
+        <div className={contactusContentstyles.contactusContainer}>
+          <button
+            type='button'
+            onClick={handleClick}
+            style={{ display: 'inline-flex', alignItems: 'center' }}
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+            >
+              <g clipPath='url(#clip0_1433_2277)'>
+                <path
+                  d='M7.825 13L13.425 18.6L12 20L4 12L12 4L13.425 5.4L7.825 11H20V13H7.825Z'
+                  fill='#546E7A'
+                />
+              </g>
+              <defs>
+                <clipPath id='clip0_1433_2277'>
+                  <rect width='24' height='24' fill='white' />
+                </clipPath>
+              </defs>
+            </svg>
+            <div className={globalStyleCss.regularLinkBlackBold}>
+              &nbsp;Back to Home page
             </div>
-            <div className={contactusContentstyles.mainContainer}>
-                <div className={contactusContentstyles.contactusContainer}>
-                    <button type='button' onClick={handleClick} style={{ display: 'inline-flex', alignItems: 'center' }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <g clipPath="url(#clip0_1433_2277)">
-                                <path d="M7.825 13L13.425 18.6L12 20L4 12L12 4L13.425 5.4L7.825 11H20V13H7.825Z" fill="#546E7A" />
-                            </g>
-                            <defs>
-                                <clipPath id="clip0_1433_2277">
-                                    <rect width="24" height="24" fill="white" />
-                                </clipPath>
-                            </defs>
-                        </svg>
-                        <div className={globalStyleCss.regularLinkBlackBold}>&nbsp;Back to Home page</div>
-                    </button>
-                    <div className={globalStyleCss.header2}>
-                        Contact Us
-                    </div>
+          </button>
+          <div className={globalStyleCss.header2}>Contact Us</div>
 
-                    <div className={contactusContentstyles.bodyContainer}>
-                        <div className={globalStyleCss.header2}>
-                            Union of Security Employees (USE)
-                        </div>
+          <div className={contactusContentstyles.bodyContainer}>
+            <div className={globalStyleCss.header2}>
+              Union of Security Employees (USE)
+            </div>
 
-                        <hr className={contactusContentstyles.hrLine}></hr>
-                        <div className={contactusContentstyles.box1}>
-                            <div className={contactusContentstyles.leftText}>
-                                <div className={globalStyleCss.regularBold}>Customer Service Centre</div>
-                            </div>
-                            <div className={contactusContentstyles.rightText}>
-                                <div className={globalStyleCss.regular}>+65 6381 9150<br></br>+65 6291 5145</div>
-                            </div>
-                        </div>
-                        <hr className={contactusContentstyles.hrLine}></hr>
-                        <div className={contactusContentstyles.box1}>
-                            <div className={contactusContentstyles.leftText}>
-                                <div className={globalStyleCss.regularBold}>Email</div>
-                            </div>
-                            <div className={contactusContentstyles.rightText}>
-                                <div className={globalStyleCss.regular}>use-idcard@ntuc.org.sg</div>
-                            </div>
-                        </div>
-                        <hr className={contactusContentstyles.hrLine}></hr>
-                        <div className={contactusContentstyles.box1}>
-                            <div className={contactusContentstyles.leftText}>
-                                <div className={globalStyleCss.regularBold}>Location</div>
-                            </div>
-                            <div className={contactusContentstyles.rightText}>
-                                <div className={globalStyleCss.regular}>Union of Security Employees (USE)
-                                    <br></br>200 Jalan Sultan
-                                    <br></br>#03-24 Textile Centre
-                                    <br></br>Singapore 199018
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+            <hr className={contactusContentstyles.hrLine}></hr>
+            <div className={contactusContentstyles.box1}>
+              <div className={contactusContentstyles.leftText}>
+                <div className={globalStyleCss.regularBold}>
+                  Customer Service Centre
                 </div>
+              </div>
+              <div className={contactusContentstyles.rightText}>
+                <div className={globalStyleCss.regular}>
+                  +65 6381 9150<br></br>+65 6291 5145
+                </div>
+              </div>
             </div>
-
-            <div >
-                <FooterPageLink />
+            <hr className={contactusContentstyles.hrLine}></hr>
+            <div className={contactusContentstyles.box1}>
+              <div className={contactusContentstyles.leftText}>
+                <div className={globalStyleCss.regularBold}>Email</div>
+              </div>
+              <div className={contactusContentstyles.rightText}>
+                <div className={globalStyleCss.regular}>
+                  use-idcard@ntuc.org.sg
+                </div>
+              </div>
             </div>
+            <hr className={contactusContentstyles.hrLine}></hr>
+            <div className={contactusContentstyles.box1}>
+              <div className={contactusContentstyles.leftText}>
+                <div className={globalStyleCss.regularBold}>Location</div>
+              </div>
+              <div className={contactusContentstyles.rightText}>
+                <div className={globalStyleCss.regular}>
+                  Union of Security Employees (USE)
+                  <br></br>200 Jalan Sultan
+                  <br></br>#03-24 Textile Centre
+                  <br></br>Singapore 199018
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-        </form>
-    );
+      <div>
+        <FooterPageLink />
+      </div>
+    </form>
+  );
 };
 
 export default ContactusPage;
